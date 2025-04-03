86132b4702833a88e7f5c07cb200d40c64cb278d
turner-burns <tcburns@sas.upenn.edu>
2025-04-03 15:54:38 -0400

    Delete setup.js

--- setup.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map', {
-  center: [42.40740580444195, -71.12013072626375],
-  zoom: 16,
-});
-
-var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
